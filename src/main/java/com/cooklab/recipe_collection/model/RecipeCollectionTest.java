64f28a6a91917e29eb3c8d47cccff32d7a40c7a4
Martin Chang <martin80699@gmail.com>
2023-09-29 10:56:29 +0800

    Martin Hibernate VO test

--- src/main/java/com/cooklab/recipe_collection/model/RecipeCollectionTest.java
+++ src/main/java/com/cooklab/recipe_collection/model/RecipeCollectionTest.java
@@ -2,10 +2,14 @@ package com.cooklab.recipe_collection.model;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
+import com.cooklab.util.HibernateUtil;
+
 public class RecipeCollectionTest {
 
 	public static void main(String[] args) {
-		RecipeCollectionJDBCDAOlm dao = new RecipeCollectionJDBCDAOlm();
+//		RecipeCollectionJDBCDAOlm dao = new RecipeCollectionJDBCDAOlm();
 //=======================insert=======================
 //		RecipeCollectionVO vo = new RecipeCollectionVO(null, 1, 1, null);
 //		dao.insert(vo);
@@ -22,7 +26,34 @@ public class RecipeCollectionTest {
 //			for (RecipeCollectionVO recipeCollection : recipeCollectionList) {
 //				System.out.println(recipeCollection);
 //			}	
-//======================================================	
-
+//==========================Hibernate============================
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			//新增
+//			RecipeCollectionVO vo = new RecipeCollectionVO(null, 1, 1, null);			
+//			session.saveOrUpdate(vo);
+			//更新
+//			RecipeCollectionVO vo = new RecipeCollectionVO(1, 2, 5, null);			
+//			session.saveOrUpdate(vo);
+			//刪除
+//			RecipeCollectionVO vo = session.get(RecipeCollectionVO.class,1);
+//			if(vo !=null) {
+//				session.delete(vo);
+//			}
+			//查詢(多筆)
+//			List<RecipeCollectionVO> list = session.createQuery("from RecipeCollectionVO ",RecipeCollectionVO.class).list();
+//			System.out.println(list);
+			//查詢(單筆)
+//			RecipeCollectionVO vo = session.createQuery("from RecipeCollectionVO where  collectionNo = 1", RecipeCollectionVO.class).uniqueResult();
+//			System.out.println(vo);
+			
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			HibernateUtil.shutdown();
+		}
 	}
 }
--- src/main/java/com/cooklab/recipe_kitchenware/model/RecipeKitchenwareVO.java
+++ src/main/java/com/cooklab/recipe_kitchenware/model/RecipeKitchenwareVO.java
@@ -2,11 +2,26 @@ package com.cooklab.recipe_kitchenware.model;
 
 import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name = "recipe_kitchenware")
 public class RecipeKitchenwareVO implements java.io.Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "recipe_kitchenware_no", updatable = false)
 	private Integer recipeKitchenwareNo;
+	@Column(name = "recipe_no")
 	private Integer recipeNo;
+	@Column(name = "product_no")
 	private Integer productNo;
+	@Column(name = "text_label")
 	private String textLabel;
+	@Column(name = "created_timestamp", insertable = false, updatable = false)
 	private Timestamp createdTimestamp;
 
 	public RecipeKitchenwareVO() {
