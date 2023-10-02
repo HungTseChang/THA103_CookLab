8fb9aa21c0917fdc1cb008d97573fc6b0474fdbc
Martin Chang <martin80699@gmail.com>
2023-10-02 20:12:56 +0800

    Martin step VO

--- src/main/java/com/cooklab/recipe_step/model/RecipeStepTest.java
+++ src/main/java/com/cooklab/recipe_step/model/RecipeStepTest.java
@@ -2,6 +2,10 @@ package com.cooklab.recipe_step.model;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
+import com.cooklab.util.HibernateUtil;
+
 public class RecipeStepTest {
 	public static void main(String[] args) {
 		RecipeStepJDBCDAOlm dao = new RecipeStepJDBCDAOlm();
@@ -21,5 +25,35 @@ public class RecipeStepTest {
 //		for (RecipeStepVO recipeStep : recipeStepList) {
 //			System.out.println(recipeStep);
 //		}
+//==========================Hibernate============================
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		try {
+			session.beginTransaction();
+			// 新增
+			RecipeStepVO vo = new RecipeStepVO(1, 1, 10, null, "步驟", null);
+			session.saveOrUpdate(vo);
+			// 更新
+//					RecipeCollectionVO vo = new RecipeCollectionVO(1, 2, 5, null);			
+//					session.saveOrUpdate(vo);
+			// 刪除
+//					RecipeCollectionVO vo = session.get(RecipeCollectionVO.class,1);
+//					if(vo !=null) {
+//						session.delete(vo);
+//					}
+			// 查詢(多筆)
+//					List<RecipeCollectionVO> list = session.createQuery("from RecipeCollectionVO ",RecipeCollectionVO.class).list();
+//					System.out.println(list);
+			// 查詢(單筆)
+//					RecipeCollectionVO vo = session.createQuery("from RecipeCollectionVO where  collectionNo = 1", RecipeCollectionVO.class).uniqueResult();
+//					System.out.println(vo);
+
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+		} finally {
+			HibernateUtil.shutdown();
+		}
 	}
+
 }
--- src/main/java/com/cooklab/recipe_step/model/RecipeStepVO.java
+++ src/main/java/com/cooklab/recipe_step/model/RecipeStepVO.java
@@ -1,15 +1,32 @@
 package com.cooklab.recipe_step.model;
 
-import java.sql.Time;
+import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.Arrays;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "recipe_step")
+@IdClass(RecipeStepVO.CompositeDetail.class)
 public class RecipeStepVO implements java.io.Serializable{
+	@Id
+	@Column(name = "recipe_no")
 	private Integer recipeNo;
+	@Id
+	@Column(name = "step")
 	private Integer step;
+	@Column(name = "step_time")
 	private Integer stepTime;
+	@Column(name = "step_img")
 	private byte[] stepImg;
+	@Column(name = "step_content")
 	private String stepContent;
+	@Column(name = "created_timestamp", insertable = false, updatable = false)
 	private Timestamp createdTimestamp;
 	
 	public RecipeStepVO() {	
@@ -24,6 +41,11 @@ public class RecipeStepVO implements java.io.Serializable{
 		this.stepImg = stepImg;
 		this.stepContent = stepContent;
 		this.createdTimestamp = createdTimestamp;
+	}
+	
+	public void setCompositeKey(CompositeDetail key) {
+		this.recipeNo = key.getRecipeNo();
+		this.step = key.getStep();
 	}
 
 	public Integer getRecipeNo() {
@@ -72,5 +94,63 @@ public class RecipeStepVO implements java.io.Serializable{
 				+ "]";
 	}
 	
+	static class CompositeDetail implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		private Integer recipeNo;
+		private Integer step;
+		
+		// 一定要有無參數建構子
+		public CompositeDetail() {
+			super();
+		}
+
+		public CompositeDetail(Integer recipeNo, Integer step) {
+			super();
+			this.recipeNo = recipeNo;
+			this.step = step;
+		}
+
+		public Integer getRecipeNo() {
+			return recipeNo;
+		}
+
+		public void setRecipeNo(Integer recipeNo) {
+			this.recipeNo = recipeNo;
+		}
+
+		public Integer getStep() {
+			return step;
+		}
+
+		public void setStep(Integer step) {
+			this.step = step;
+		}
+
+		// 一定要 override 此類別的 hashCode() 與 equals() 方法！
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((recipeNo == null) ? 0 : recipeNo.hashCode());
+			result = prime * result + ((step == null) ? 0 : step.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+
+			if (obj != null && getClass() == obj.getClass()) {
+				CompositeDetail compositeKey = (CompositeDetail) obj;
+				if (recipeNo.equals(compositeKey.recipeNo) && step.equals(compositeKey.step)) {
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
 	
 }
