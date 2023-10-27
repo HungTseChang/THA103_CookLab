98d1448947efddfb92ae271cb5423d7e80ec1b1b
Horek <z8010272003@gmail.com>
2023-10-27 19:20:57 +0800

    按讚區域:按鈕顯示功能完成，功能製作中

--- src/main/java/com/cooklab/article_reaction/model/ArticleReactionService.java
+++ src/main/java/com/cooklab/article_reaction/model/ArticleReactionService.java
@@ -14,10 +14,23 @@ public class ArticleReactionService {
 	public void update(ArticleReactionVO ArticleReactionVO) {
 		dao.update(ArticleReactionVO);
 	}
+	
+	public void create(Integer memberId ,Integer articleNo ,Byte status ) {
+		ArticleReactionVO exist =dao.findByTwoCol(memberId,articleNo);
+		
+		exist.setArticleNo(articleNo);
+		exist.setMemberId(memberId);
+		exist.setStatuts(status);
+		dao.update(exist);
+	}
+	
 	//下面是一次用兩個欄位收尋
 	public ArticleReactionVO findTwo (Integer memberId , Integer articleNo) {
 		return dao.findByTwoCol(memberId, articleNo);
 	}
+	
+
+	
 	
 	public ArticleReactionVO getOne (Integer articleReactionNo ) {
 		return dao.findByPrimaryKey(articleReactionNo);
