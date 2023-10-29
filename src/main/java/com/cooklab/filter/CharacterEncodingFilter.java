9e45896d244d1721d7c9cad27f2b6499b03fa5f3
Martin Chang <martin80699@gmail.com>
2023-10-29 14:48:56 +0800

    master 專題整合 merge

--- src/main/java/com/cooklab/filter/CharacterEncodingFilter.java
+++ src/main/java/com/cooklab/filter/CharacterEncodingFilter.java
@@ -6,29 +6,23 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import java.io.IOException;
+import javax.servlet.annotation.WebFilter;
 
+import java.io.IOException;
+@WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        // 初始化过滤器（可选）
     }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        // 设置请求字符编码
     	req.setCharacterEncoding("UTF-8");
-
-        // 设置响应Content-Type头
         res.setContentType("text/html; charset=UTF-8");
-
-        // 继续处理请求
         chain.doFilter(req, res);
     }
-
     @Override
     public void destroy() {
-        // 销毁过滤器（可选）
     }
 }
 
--- src/main/java/com/cooklab/listener/SingletonServletContextListener.java
+++ src/main/java/com/cooklab/listener/SingletonServletContextListener.java
@@ -2,10 +2,11 @@ package com.cooklab.listener;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
 import com.cooklab.util.HibernateUtil;
 import com.cooklab.util.JedisUtil;
-
+@WebListener
 public class SingletonServletContextListener implements ServletContextListener {
 
     @Override
@@ -18,7 +19,6 @@ public class SingletonServletContextListener implements ServletContextListener {
     public void contextDestroyed(ServletContextEvent event) {
     	HibernateUtil.shutdown();
     	JedisUtil.shutdownJedisPool();
-
     }
 }
 
